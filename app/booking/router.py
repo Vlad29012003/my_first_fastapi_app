c4cf03dcbe57c21b5a0eb0deb8aaa470d409825a
Vlad <vladosmen29@gmail.com>
2023-12-08 22:35:58 +0600

    added sql alhemy request

--- app/booking/router.py
+++ app/booking/router.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter ,Depends
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 from app.booking.service import BookingService
+from datetime import datetime, date, time
+
 from typing import List
 
 router = APIRouter(
@@ -17,6 +19,17 @@ async def get_bookings(user: Users = Depends(get_current_user)): #-> List[Sbooki
     return await BookingService.find_all(user_id=1)
     # return await BookingService.find_all()
 
+@router.get('')
+async def add_booking(
+    user:Users = Depends(get_current_user),
+):
+    await BookingService.add(user_id = user.id)
 
 
 
+@router.post('')
+async def add_booking(
+    room_id: int, date_from: date, date_to:date,
+    user:Users = Depends(get_current_user),
+):
+    await BookingService.add(user.id, room_id,date_from,date_to)
--- app/booking/service.py
+++ app/booking/service.py
@@ -1,11 +1,57 @@
 
 
 from app.database import asynch_session_maker
-from sqlalchemy import select
+from sqlalchemy import select , delete , insert , func , and_ , or_
 from app.booking.models import Booking
 from app.services.base import BaseService
+from datetime import datetime, date, time
+
 
 class BookingService(BaseService):
     model = Booking
+
+    @classmethod
+    async def add(
+        cls,
+        room_id: int,
+        date_from: date,
+        date_to: date,
+    ):
+        """
+        WITH booked_rooms AS (
+            SELECT * FROM bookings
+            WHERE room_id = 1 AND
+            (date_from >= '2023-05-15' AND date_from <= '2023-06-20) OR
+            (date_from <= '2023-05-15' AND date_to > '2023-06-20')
+        )
+        SELECT rooms.quantity - COUNT(booked_rooms.room_id) FROM rooms
+        LEFT JOIN booked_rooms ON booked_rooms.room_id = rooms.id
+        WHERE rooms.id = 1
+        GROUP BY rooms.quantity, booked_rooms.room_id
+        """
+        booked_rooms = select(Booking).where(
+            and_(
+                Booking.room_id == 1,
+                or_(
+                    and_(
+                        Booking.date_from >= date_from,
+                        Booking.date_from <= date_to
+                    ),
+                    and_(
+                        Booking.date_from <= date_from,
+                        Booking.date_to > date_from,
+                    ),
+                )
+            )
+        ).cte('booked_rooms')
+
+        """
+        SELECT rooms.quantity - COUNT(booked_rooms.room_id) FROM rooms
+        LEFT JOIN booked_rooms ON booked_rooms.room_id = rooms.id
+        WHERE rooms.id = 1
+        GROUP BY rooms.quantity, booked_rooms.room_id
+        """
+
+        rooms_left = select(Rooms.quantity - COUNT(booked_rooms.room_id)).select_from(room_id)
 
     
